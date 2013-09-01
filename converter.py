f1feb304078b96c1ac86813cbd363bc097f7fcbd
Ash <lukasz.jakub.adamczuk@gmail.com>
2013-09-01 23:49:12 +0200

    Update converter.py

--- converter.py
+++ converter.py
@@ -5,7 +5,7 @@ import os, sys
 input = open(sys.argv[1])
 output = open(sys.argv[2], 'w')
 
-subs = input.read().decode("Windows-1250")
+subs = input.read().decode('Windows-1250')
 output.write(subs.encode('utf-8'))
 
 output.close()
